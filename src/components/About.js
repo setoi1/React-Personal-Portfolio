0fe616db664e0cbae658e138a31b341b26825356
Ian <i.seto00@gmail.com>
2021-03-24 00:29:13 -0400

    updated About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+
 const About = () => {
     return (
       <>
-        <div id="about-section">
-          <div className="welcome-text">
-            <h4>About</h4>
+        <div className="about-section">
+          <div className="header-text">
+            <h3>About</h3>
           </div>
         </div>
       </>
