37cb598c2cc71f81ae8a77ac865d4d8c52cacd74
Ian <i.seto00@gmail.com>
2021-03-24 00:31:01 -0400

    updated Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Card from '../components/Card';
+import Row from '../components/Row';
+import { Grid } from '@material-ui/core';
 
 const Projects = () => {
-    return (
-      <>
-        <div id="projects-section">
-          <div className="welcome-text">
-            <h4>Projects</h4>
-          </div>
-        </div>
-      </>
-    )
-}
+  return (
+    <div className="projects-section">
+      <h3 className="header-text">Projects</h3>
+      <Grid className="project-grid-container" container spacing={1}>
+        <Grid className="project-row-container" container item xs={3} spacing={4}>
+          <Row />
+          <Row />
+          <Row />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default Projects;
