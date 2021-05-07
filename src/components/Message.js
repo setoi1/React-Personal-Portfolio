c52a8d660a7b214e880e520712a8eb1ae3ea9a56
Ian <i.seto00@gmail.com>
2021-05-07 18:47:06 -0400

    Added Message page

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Message = () => {
+  return (
+    <div className="message-page">
+      <div className="header-text">
+        <h3>Contact Me</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Message;
