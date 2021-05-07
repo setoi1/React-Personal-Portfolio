a2e1aa9da4a69e1a42b189540cee133de64c0956
Ian <i.seto00@gmail.com>
2021-05-07 18:47:25 -0400

    Removed activeClass

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,20 @@ const Navbar = () => {
     <AppBar>
       <Toolbar className="default-navbar-nav">
         <LanguageIcon className="navbar-icon" />
-        <Link className="default-navbar-navlink" activeClass="header" to="landing-section" spy={true} smooth={true} duration={750}>
+        <Link className="default-navbar-navlink"  to="landing-section" spy={true} smooth={true} duration={750}>
           <Typography>Home</Typography>
         </Link>
-        <Link className="default-navbar-navlink" activeClass="header" to="about-section" spy={true} smooth={true} duration={750}>
+        <Link className="default-navbar-navlink"  to="about-section" spy={true} smooth={true} duration={750}>
           <Typography>About</Typography>
         </Link>
-        <Link className="default-navbar-navlink" activeClass="header" to="projects-section" spy={true} smooth={true} duration={750}>
+        <Link className="default-navbar-navlink"  to="projects-section" spy={true} smooth={true} duration={750}>
           <Typography>Projects</Typography>
         </Link>
-        <Link className="default-navbar-navlink" activeClass="header" to="contacts-section" spy={true} smooth={true} duration={750}>
+        <Link className="default-navbar-navlink"  to="contacts-section" spy={true} smooth={true} duration={750}>
           <Typography>Contacts</Typography>
+        </Link>
+        <Link className="default-navbar-navlink"  to="message-page" spy={true} smooth={true} duration={750}>
+          <Typography>Message</Typography>
         </Link>
       </Toolbar>
     </AppBar>
