27127984b4ff38b65788d4bfa1eca618cd9da1a3
Ian <i.seto00@gmail.com>
2021-05-07 18:48:06 -0400

    Changed Row to Grid

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import Row from '../components/Row';
 import { Grid } from '@material-ui/core';
+import ProjectCard from './ProjectCard';
 
 const Projects = () => {
+
   return (
     <div className="projects-section">
       <h3 className="header-text">Projects</h3>
-      <Grid className="project-grid-container" container spacing={1}>
-        <Grid className="project-row-container" container item xs={3} spacing={4}>
-          <Row />
-          <Row />
-          <Row />
+      <div className="projects-container">
+        <Grid className="projects-container-items" container spacing={10}>
+          <Grid item>
+            <ProjectCard 
+              title="Finance Manager" 
+              techStack="Python" 
+              description="A simple Finance Managing Application built with Python and tkinter" 
+              link="https://github.com/marinom1/FiMan-Financial_Manager" 
+            />
+          </Grid>
+          <Grid item>
+            <ProjectCard 
+              title="Business Manager" 
+              techStack="JavaScript, React"
+              description="Work in Progress"
+            />
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </div>
   );
 };
+
+
+
+/*
+      <Grid className="project-grid-container" container spacing={1}>
+        <Grid className="project-row-container" container item xs={4} spacing={6}>
+          <Row />
+        </Grid>
+      </Grid>
+*/
 
 export default Projects;
