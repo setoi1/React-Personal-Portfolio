63fac399a5d500365c83b6b4c4aa91b469c39b0d
Ian <i.seto00@gmail.com>
2021-05-10 04:21:09 -0400

    Changed landing header and added page transition

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-scroll';
+import { Button } from '@material-ui/core';
+import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle';
 
 const Landing = () => {
   return (
     <div className="landing-section">
-      <div className="header-text">
-        <h3>Welcome</h3>
+      <div className="landing-container">
+        <div className="section-header-text">
+          <h3>Explore</h3>
+        </div>
+        <div className="page-transition">
+          <Link className="about-section-link"  to="about-section" spy={true} smooth={true} duration={750}>
+            <Button className="about-arrow-button"> 
+              <ArrowDropDownCircleIcon className="about-arrow-icon" />
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
