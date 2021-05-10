6d33010fc4f27645197b7bc69f45d0f0b87f2cf4
Ian <i.seto00@gmail.com>
2021-05-10 04:23:12 -0400

    Updated section header text class name

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Message = () => {
   return (
     <div className="message-page">
-      <div className="header-text">
+      <div className="section-header-text">
         <h3>Contact Me</h3>
       </div>
     </div>
