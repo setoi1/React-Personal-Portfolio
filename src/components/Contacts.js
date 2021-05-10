d344ff464d7b91d488b18a3396e58abf9a4e8b65
Ian <i.seto00@gmail.com>
2021-05-10 04:20:27 -0400

    Added Contact section information

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import MailIcon from '@material-ui/icons/Mail';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Contacts = () => {
   return (
     <div className="contacts-section">
-      <div className="header-text">
+      <div className="section-header-text">
         <h3>Contacts</h3>
+      </div>
+      <div className="contacts-container">
+        <ul className="contacts-list">
+          <li className="contacts-list-item">
+            <a className="contacts-item-link" href="">
+              <MailIcon />
+            </a>
+          </li>
+          <li className="contacts-list-item">
+            <a className="contacts-item-link" href="https://www.linkedin.com/in/ianseto/" target="_blank">
+              <LinkedInIcon />
+            </a>
+          </li>
+          <li className="contacts-list-item">
+            <a className="contacts-item-link" href="https://github.com/setoi1" target="_blank">
+              <GitHubIcon />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
