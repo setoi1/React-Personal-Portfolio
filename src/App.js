44cd8eed35c1e1c8d2fd4088fbd1e2880d600ad4
Ian <i.seto00@gmail.com>
2021-05-10 04:19:40 -0400

    Removed Message.js import

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contacts from './components/Contacts';
 import Footer from './components/Footer';
+import Message from './components/Message';
 
 function App() {
   return (
