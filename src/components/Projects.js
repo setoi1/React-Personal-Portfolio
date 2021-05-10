cff86128c214d26f7f3bc2848703efb4e97fd1d7
Ian <i.seto00@gmail.com>
2021-05-10 04:22:54 -0400

    Added Button, Link, and Icon imports, added page transition

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 import ProjectCard from './ProjectCard';
+import { Link } from 'react-scroll';
+import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle';
 
 const Projects = () => {
 
   return (
     <div className="projects-section">
-      <h3 className="header-text">Projects</h3>
       <div className="projects-container">
+        <div className="section-header-text">
+          <h3>Projects</h3>
+        </div>
         <Grid className="projects-container-items" container spacing={10}>
           <Grid item>
             <ProjectCard 
@@ -25,6 +29,13 @@ const Projects = () => {
             />
           </Grid>
         </Grid>
+        <div className="page-transition">
+          <Link className="contacts-section-link"  to="contacts-section" spy={true} smooth={true} duration={750}>
+            <Button className="contacts-arrow-button"> 
+              <ArrowDropDownCircleIcon className="contacts-arrow-icon" />
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
