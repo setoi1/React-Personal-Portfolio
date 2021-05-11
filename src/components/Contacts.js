aaadeac57abbb3c760d6f117d89004ac01534d80
Ian <i.seto00@gmail.com>
2021-05-10 21:08:34 -0400

    Created contacts container

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -6,25 +6,25 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 const Contacts = () => {
   return (
     <div className="contacts-section">
-      <div className="section-header-text">
-        <h3>Contacts</h3>
-      </div>
       <div className="contacts-container">
-        <ul className="contacts-list">
-          <li className="contacts-list-item">
-              <MailIcon />
-          </li>
-          <li className="contacts-list-item">
-            <a className="contacts-item-link" href="https://www.linkedin.com/in/ianseto/" rel="noreferrer" target="_blank">
-              <LinkedInIcon />
-            </a>
-          </li>
-          <li className="contacts-list-item">
-            <a className="contacts-item-link" href="https://github.com/setoi1" rel="noreferrer" target="_blank">
-              <GitHubIcon />
-            </a>
-          </li>
-        </ul>
+        <div className="section-header-text">
+          <h3>Contacts</h3>
+        </div>
+          <ul className="contacts-list">
+            <li className="contacts-list-item">
+                <MailIcon className="contacts-item-link"/>
+            </li>
+            <li className="contacts-list-item">
+              <a className="contacts-item-link" href="https://www.linkedin.com/in/ianseto/" rel="noreferrer" target="_blank">
+                <LinkedInIcon />
+              </a>
+            </li>
+            <li className="contacts-list-item">
+              <a className="contacts-item-link" href="https://github.com/setoi1" rel="noreferrer" target="_blank">
+                <GitHubIcon />
+              </a>
+            </li>
+          </ul>
       </div>
     </div>
   );
