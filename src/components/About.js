9daf94c34bff3007229c07cbebe85406d24edee8
Ian <i.seto00@gmail.com>
2021-05-11 17:24:27 -0400

    Updated about description

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
         </div>
         <div className="about-description">
           <p>
-            Hello! My name is Ian Seto, currently a 4th year student at Wentworth Institute of Technology studying Computer Science for my major. I am extremely passionate for building well-designed and robust applications using various languages such as JavaScript, React, Java, and Python.
+            Hello! My name is Ian Seto, currently a 4th year student at Wentworth Institute of Technology studying Computer Science for my major. I am passionate for learning new technologies and building well-designed applications using various languages such as JavaScript, React, Java, and Python.
           </p>
         </div>
         <div className="page-transition">
