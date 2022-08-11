7680bcb9a0ca355cce3b7efdcfe5e9f32c400b9c
Ian <i.seto00@gmail.com>
2022-08-11 17:59:40 -0400

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
           <h3>About</h3>
         </div>
         <div className="about-description">
-          <p>
+          <p className="about-description-paragraph">
             Hi! My name is Ian Seto and I'm a recent graduate from Wentworth Institute of Technology with a bachelors degree in Computer Sciecne. I'm passionate for learning new technologies and building well-designed applications using various languages such as JavaScript / TypeScript, React, Java, and Python. A few of my hobbies include gaming, traveling, and listening to music.
           </p>
         </div>
